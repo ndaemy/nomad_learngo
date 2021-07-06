a3aa01c4355add37682694b576fff2934f72ab29
ndaemy <yuyaebean@gmail.com>
2021-07-07 00:15:22 +0900

    2.6 Update Delete

--- main.go
+++ main.go
@@ -7,17 +7,15 @@ import (
 )
 
 func main() {
-	dictionary := mydict.Dictionary{"first": "First word"}
-	word := "hello"
-	definition := "Greeting"
-	err := dictionary.Add(word, definition)
+	dictionary := mydict.Dictionary{}
+	baseWord := "hello"
+	dictionary.Add(baseWord, "First")
+	dictionary.Search(baseWord)
+	dictionary.Delete(baseWord)
+	word, err := dictionary.Search(baseWord)
 	if err != nil {
 		fmt.Println(err)
-	}
-	hello, _ := dictionary.Search(word)
-	fmt.Println(hello)
-	err2 := dictionary.Add(word, definition)
-	if err2 != nil {
-		fmt.Println(err2)
+	} else {
+		fmt.Println(word)
 	}
 }
--- mydict/dict.go
+++ mydict/dict.go
@@ -5,8 +5,11 @@ import "errors"
 // Dictionary type
 type Dictionary map[string]string
 
-var errNotFound = errors.New("not found")
-var errWordExists = errors.New("word already exists")
+var (
+	errNotFound   = errors.New("not found")
+	errCantUpdate = errors.New("can't update non-existing word")
+	errWordExists = errors.New("word already exists")
+)
 
 // Search for a word
 func (d Dictionary) Search(word string) (string, error) {
@@ -18,7 +21,7 @@ func (d Dictionary) Search(word string) (string, error) {
 }
 
 // Add a word to the dictionary
-func (d Dictionary) Add(word , def string) error {
+func (d Dictionary) Add(word, def string) error {
 	_, err := d.Search(word)
 	switch err {
 	case errNotFound:
@@ -27,4 +30,21 @@ func (d Dictionary) Add(word , def string) error {
 		return errWordExists
 	}
 	return nil
+}
+
+// Update a word
+func (d Dictionary) Update(word, def string) error {
+	_, err := d.Search(word)
+	switch err {
+	case nil:
+		d[word] = def
+	case errNotFound:
+		return errCantUpdate
+	}
+	return nil
+}
+
+// Delete a word
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
 }
