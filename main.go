7f2806d0ee0ca192a95012f10c8027139249c62f
ndaemy <yuyaebean@gmail.com>
2021-07-13 23:02:41 +0900

    3.5 One more Recap

--- main.go
+++ main.go
@@ -1,23 +1,46 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"time"
+	"net/http"
 )
 
-func isCool(person string, c chan string) {
-	time.Sleep(time.Second * 5)
-	fmt.Println(person)
-	c <- person + " is cool"
-}
+var errRequestFailed = errors.New("request failed")
 
 func main() {
-	c := make(chan string)
-	people := [4]string{"ndaemy", "soultree", "fly", "captain"}
-	for _, person := range people {
-		go isCool(person, c)
+	var results = make(map[string]string)
+
+	urls := []string{
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://www.google.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://instagram.com/",
+		"https://nomadcoders.co/",
 	}
-	for range people {
-		fmt.Println(<-c)
+	for _, url := range urls {
+		result := "OK"
+		err := hitURL(url)
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
 	}
+	for url, result := range results {
+		fmt.Println(url, result)
+	}
+}
+
+func hitURL(url string) error {
+	fmt.Println("Checking:", url)
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode >= 400 {
+		fmt.Println(err, resp.StatusCode)
+		return errRequestFailed
+	}
+	return nil
 }
