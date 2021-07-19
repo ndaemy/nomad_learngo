2e595c11890449682249e235a765f633634ac057
ndaemy <yuyaebean@gmail.com>
2021-07-19 20:37:21 +0900

    4.3 extractJob part Two

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package main
+package main2
 
 import (
 	"fmt"
@@ -20,14 +20,19 @@ type extractedJob struct {
 var baseURL = "https://kr.indeed.com/jobs?q=golang"
 
 func main() {
+	var jobs []extractedJob
 	totalPages := getPages()
 
 	for i := 0; i < totalPages; i++ {
-		getPage(i)
+		extractedJobs := getPage(i)
+		jobs = append(jobs, extractedJobs...)
 	}
+
+	fmt.Println(jobs)
 }
 
-func getPage(page int) {
+func getPage(page int) []extractedJob {
+	var jobs []extractedJob
 	pageURL := baseURL + "&start=" + strconv.Itoa(page*10)
 	fmt.Println("Requesting", pageURL)
 	res, err := http.Get(pageURL)
@@ -39,13 +44,27 @@ func getPage(page int) {
 
 	searchCards := doc.Find(".resultWithShelf")
 	searchCards.Each(func(i int, card *goquery.Selection) {
-		id, _ := card.Attr("data-jk")
-		title := card.Find(".jobTitle").Text()
-		company := card.Find(".companyName").Text()
-		companyLocation := card.Find(".companyLocation").Text()
-
-		fmt.Println(id, title, company, companyLocation)
+		job := extractJob(card)
+		jobs = append(jobs, job)
 	})
+
+	return jobs
+}
+
+func extractJob(card *goquery.Selection) extractedJob {
+	id, _ := card.Attr("data-jk")
+	title := card.Find(".jobTitle").Text()
+	company := card.Find(".companyName").Text()
+	companyLocation := card.Find(".companyLocation").Text()
+	summary := card.Find(".job-snippet").Text()
+
+	return extractedJob{
+		id:       id,
+		title:    title,
+		company:  company,
+		location: companyLocation,
+		summary:  summary,
+	}
 }
 
 func getPages() int {
