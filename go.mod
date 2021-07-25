710fa43e4015452af0051fda623d3cf8d1543a6f
ndaemy <yuyaebean@gmail.com>
2021-07-25 23:05:58 +0900

    5.1 Setup part Two

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module nomad_learngo
 go 1.16
 
 require (
-	github.com/PuerkitoBio/goquery v1.7.1 // indirect
-	github.com/labstack/echo/v4 v4.4.0 // indirect
+	github.com/PuerkitoBio/goquery v1.7.1
+	github.com/labstack/echo/v4 v4.4.0
 )
--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
+import (
+	"strings"
+
+	"github.com/labstack/echo/v4"
+)
+
+func handleHome(c echo.Context) error {
+	return c.File("home.html")
+}
+
+func handleScrape(c echo.Context) error {
+	term := strings.ToLower(cleanString(c.FormValue("term")))
+	return nil
+}
+
+func cleanString(str string) string {
+	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
+}
+
 func main() {
-	Scrape("python")
+	e := echo.New()
+	e.GET("/", handleHome)
+	e.POST("/scrape", handleScrape)
+	e.Logger.Fatal(e.Start(":1323"))
 }
