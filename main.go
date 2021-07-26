bbf408ec306ea96add62ef8b0f424595f79c6573
ndaemy <yuyaebean@gmail.com>
2021-07-26 19:49:07 +0900

    5.2 File Download

--- main.go
+++ main.go
@@ -1,18 +1,23 @@
 package main
 
 import (
+	"os"
 	"strings"
 
 	"github.com/labstack/echo/v4"
 )
+
+const fileName = "jobs.csv"
 
 func handleHome(c echo.Context) error {
 	return c.File("home.html")
 }
 
 func handleScrape(c echo.Context) error {
+	defer os.Remove(fileName)
 	term := strings.ToLower(cleanString(c.FormValue("term")))
-	return nil
+	Scrape(term)
+	return c.Attachment(fileName, fileName)
 }
 
 func cleanString(str string) string {
